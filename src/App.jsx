31e033e1a416050b9f706adcb60992be1c8284f1
carry2on <manankpandya307@gmail.com>
2025-07-07 14:13:00 +0530

    just adding the portfolio in the projext for the verifivaion of the other codder

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ function App() {
 <h1 className='text-5xl font-bold text-gray-800'>AI Image Enhancer</h1>
     <p className='text-lg text-gray-500 pt-3 '>Powered by Manank</p>
      </div>
-     
-     header
+  
       <Home />
       <div className='text-sm text-gray-500 mt-6'>
         Welcome to the Image Enhancer App
         Upload your images and enhance them with ease!
       </div>
-    footer
+ MANANK THE SOFTWARE DEVLOPER PORTFIOLIO <a href="https://portfolio-manank.vercel.app/"> click me</a>
     </div></>
   )
 }
